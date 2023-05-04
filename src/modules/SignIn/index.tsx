1571421dc0bb00c9336cda70092b3139e8593088
Giovanni Nespoli <ggnespoli@gmail.com>
2023-05-03 21:08:44 -0300

    feat: signin screen

--- src/modules/SignIn/index.tsx
+++ src/modules/SignIn/index.tsx
@@ -1,20 +1,54 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useRef } from 'react';
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthParamList, auth } from '@routes/routesPath';
-import { Container } from './styles';
+import { Container, NavigationText, SignInContainer, SignInForm, NavigationContent } from './styles';
 import { Content } from '@components/Content';
+import { Header } from '@components/Header';
+import { Button } from '@components/Button';
 import { Input } from '@components/Input';
+import { RFValue } from 'react-native-responsive-fontsize';
+import theme from '@styles/theme';
 
 export function SignIn() {
     const { navigate } = useNavigation<StackNavigationProp<AuthParamList, auth>>();
+    const formRef = useRef(null);
 
     return (
         <Container>
             <Content>
-                <Input
-                    name='test'
-                />
+                <SignInContainer>
+                    <Header
+                        subTitle='Preencha os campos para entrar'
+                        title='Seja Bem-vindo(a)' />
+                    <SignInForm ref={formRef} onSubmit={() => { }}>
+                        <Input
+                            name='email'
+                            placeholder='Email'
+                        />
+                        <Input
+                            name='password'
+                            placeholder='Senha'
+                            secureTextEntry={true}
+                        />
+                        <Button
+                            label='Login'
+                            fontSize={22}
+                        />
+                    </SignInForm>
+                    <NavigationContent>
+                        <NavigationText>Não possui uma conta ?</NavigationText>
+                        <Button
+                            style={{
+                                width: "70%",
+                                height: RFValue(39),
+                                backgroundColor: theme.colors.tertiary,
+                            }}
+                            label='Cadastre-se'
+                            fontSize={16}
+                        />
+                    </NavigationContent>
+                </SignInContainer>
             </Content>
         </Container>
     );
--- src/modules/SignIn/styles.ts
+++ src/modules/SignIn/styles.ts
@@ -1,9 +1,33 @@
 import theme from "@styles/theme";
 import styled from "styled-components/native"
+import { Form } from '@unform/mobile';
 
 export const Container = styled.View`
     flex: 1;
     justify-content: center;
     align-items: center;
     background-color: ${theme.colors.primary};
-`; 
+`;
+
+export const SignInForm = styled(Form)`
+    width: 100%;
+    height: 45%;
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+export const SignInContainer = styled.View`
+    margin: auto;
+    width: 100%;
+    height: 75%;
+    align-items: center;
+    justify-content: center;
+`;
+export const NavigationText = styled.Text``;
+
+export const NavigationContent = styled.View`
+    width: 100%;
+    height: 13%;
+    align-items: center;
+    justify-content: space-between; 
+`;
