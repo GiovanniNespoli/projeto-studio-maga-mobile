1297483fd7afcb235c095a3247823f0b6fcbdd3b
Giovanni Nespoli <ggnespoli@gmail.com>
2023-05-03 21:10:05 -0300

    feat: fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Image } from 'react-native';
 import Routes from '@routes/index.routes';
+import { useFonts, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import { Outfit_500Medium, Outfit_700Bold } from '@expo-google-fonts/outfit';
 
 export default function App() {
-  return (
-    <Routes />
-  );
+  let [fontsLoaded] = useFonts({
+    Montserrat_700Bold,
+    Outfit_500Medium,
+    Outfit_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <Image source={require("@assets/logo.png")} />
+  } else {
+    return (
+      <Routes />
+    );
+  }
 }
 
 
