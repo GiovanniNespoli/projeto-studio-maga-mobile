7b6469977836307e4503884e6a08cf2025e0fae2
Giovanni Nespoli <ggnespoli@gmail.com>
2023-05-03 21:09:34 -0300

    feat: new theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,20 @@ export default {
     colors: {
         primary: '#FFF',
         secondary: '#DAA296',
-        tertiary: '#AB7C63'
+        tertiary: '#AB7C63',
+        input: "#F3F3F3",
+    },
+    text: {
+        montserrat: {
+            bold: "Montserrat_700Bold",
+        },
+        outfit: {
+            medium: "Outfit_500Medium",
+            bold: "Outfit_700Bold",
+        },
+        color: {
+            primary: "#666666",
+            secondary: "#979797",
+        }
     }
 }
