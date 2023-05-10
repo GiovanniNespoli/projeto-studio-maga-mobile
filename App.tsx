b9852027316132c10fd54408ca86f18351c0bf65
diegopetille <diegopetillecardoso@gmail.com>
2023-05-10 20:07:40 -0300

    icon added

--- App.tsx
+++ App.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Routes from '@routes/index.routes';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  if (isLoading) {
+    return <Image source={require("@assets/{colocar loading}.png")}/>
+  }
   return (
     <Routes />
   );
