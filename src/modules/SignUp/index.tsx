24822a84cb42839a9f23530a5d698bb30b24a212
Giovanni Nespoli <ggnespoli@gmail.com>
2023-05-17 21:22:28 -0300

    feat: signup module

--- src/modules/SignUp/index.tsx
+++ src/modules/SignUp/index.tsx
@@ -3,18 +3,35 @@ import React from 'react';
 import { View, Text, TouchableOpacity } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AuthParamList, auth } from '@routes/routesPath';
+import { AccontNavigationContent, Container, Grid, SignUpForm } from './styles';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { Header } from '@components/Header';
+import { Input } from '@components/Input';
+import { AccontNavigation } from '@components/AccontNavigation';
 
 export function SignUp() {
 
     const { navigate } = useNavigation<StackNavigationProp<AuthParamList, auth>>();
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity
-                style={{ backgroundColor: "red", padding: 5 }}
-                onPress={() => navigate('signin')}>
-                <Text>SIGNUP</Text>
-            </TouchableOpacity>
-        </View>
+        <KeyboardAwareScrollView>
+            <Container>
+                <Grid>
+                    <Header
+                        title='Seja Bem-vindo(a)'
+                        subTitle='Preencha os campos para cadastrar-se'
+                    />
+                    <SignUpForm>
+                        <Input name='name' />
+                        <Input name='email' />
+                        <Input name='password' />
+                        <Input name='phone' />
+                    </SignUpForm>
+                    <AccontNavigationContent>
+                        <AccontNavigation navigation='signin' />
+                    </AccontNavigationContent>
+                </Grid>
+            </Container>
+        </KeyboardAwareScrollView>
     );
 };
---
+++ src/modules/SignUp/styles.ts
@@ -0,0 +1,22 @@
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex: 1;
+    align-items: center;
+    justify-content: center;
+    background-color: #fff;
+`;
+
+export const SignUpForm = styled.View`
+
+`;
+
+export const Grid = styled.View`
+    width: 90%;
+    height: 100%;
+`;
+
+export const AccontNavigationContent = styled.View`
+    width: 100%;
+`;
