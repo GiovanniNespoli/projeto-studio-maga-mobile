805cc1b955063f838e3fda471765c4df9fa04b65
Giovanni Andrade Nespoli <ggnespoli@gmail.com>
2023-06-07 18:50:20 -0300

    full screen bb

--- src/modules/HomeTab/Appointments/NewAppointment/styles.ts
+++ src/modules/HomeTab/Appointments/NewAppointment/styles.ts
@@ -1,17 +1,24 @@
 import theme from "@styles/theme";
 import { RFValue } from "react-native-responsive-fontsize";
 import styled from "styled-components/native";
-import DropDownPicker from "react-native-dropdown-picker";
+import HorizontalDatePicker from "@logisticinfotech/react-native-horizontal-date-picker";
 
 export const Container = styled.View`
   justify-content: flex-start;
   align-items: center;
   flex: 1;
+  background-color: ${theme.colors.primary};
+`;
+export const Grid = styled.View`
+  width: 100%;
+  height: 90%;
+  align-items: center;
+  justify-content: space-around;
+  margin: auto;
 `;
 export const Header = styled.View`
   width: 100%;
   height: ${RFValue(100)}px;
-  background-color: red;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
@@ -22,9 +29,31 @@ export const HeaderTitle = styled.Text`
   font-size: ${RFValue(19)}px;
   color: ${theme.colors.textHeader};
 `;
-export const SelectContent = styled.View``;
+export const SelectContent = styled.View`
+  width: 100%;
+`;
 export const Title = styled.Text`
   font-family: ${theme.text.montserrat.bold};
   font-size: ${RFValue(20)}px;
   color: ${theme.colors.title};
+`;
+
+export const DatePicker = styled(HorizontalDatePicker)`
+  background-color: ${theme.text.color.secondary};
+`;
+
+export const TotalValueContent = styled.View`
+  width: 100%;
+  height: ${RFValue(60)}px;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const TotalValue = styled.Text`
+  font-family: ${theme.text.montserrat.bold};
+  font-size: ${RFValue(16)}px;
+  color: ${theme.colors.tertiary};
+  background-color: ${theme.colors.input};
+  padding: ${RFValue(10)}px;
+  border-radius: ${RFValue(12)}px;
 `;
